c237cfec1c53d1fd3fda0c485a61ffd1ef290a7e
Sergey Dianov <sergik5849@gmail.com>
2023-11-14 01:27:38 +0300

    init

---
+++ js/bmi.js
@@ -0,0 +1,51 @@
+"use strict"
+const button = document.getElementById('calculateButton')
+
+if (button) {
+    button.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        const height = document.getElementById('heightInput');
+        const weight = document.getElementById('weightInput');
+        const age = document.getElementById('ageInput');
+        const gender = document.getElementById('genderInput');
+
+        const parseHeight = parseFloat(height.value)
+        const parseWeight = parseFloat(weight.value)
+        const parseAge = parseInt(age.value)
+        const parseGenger = gender.value.toLowerCase()
+
+        if (isNaN(parseHeight) || isNaN(parseWeight) || isNaN(parseAge) || (parseGenger !== 'м' && parseGenger !== 'ж')) {
+            document.getElementById('resultOutput').value = `Пожалуйста, введите корректные данные.`;
+            return;
+        }
+
+        const bmi = calculateBMI(parseHeight, parseWeight);
+        const bmiCategory = getBMICategory(bmi);
+
+        document.getElementById('resultOutput').value = `ИМТ: ${bmi.toFixed(1)}, ${bmiCategory}`;
+
+        height.value = ''
+        age.value = ''
+        weight.value = ''
+        gender.value = ''
+    });
+
+    function calculateBMI(height, weight) {
+        const heightInMeters = height / 100;
+        return weight / (heightInMeters * heightInMeters);
+    }
+
+    function getBMICategory(bmi) {
+        if (bmi < 18.5) {
+            return 'дефицит массы тела';
+        } else if (bmi >= 18.5 && bmi < 25) {
+            return 'Норма';
+        } else if (bmi >= 25 && bmi < 30) {
+            return 'Избыточная масса тела';
+        } else {
+            return 'Ожирение';
+        }
+    }
+}
+
---
+++ js/main.js
@@ -0,0 +1,70 @@
+"use strict"
+
+const swiperPrev = document.querySelector('.trainings__left_arrow')
+const swiperNext = document.querySelector('.trainings__right_arrow')
+
+const mySwiper = new Swiper(".swiper", {
+    spaceBetween: 29,
+    slidesPerView: 3,
+    // centeredSlides: true,
+    roundLengths: true,
+    loop: true,
+
+    breakpoints: {
+        1450: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        },
+        900: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+
+        730: {
+            slidesPerView: 2,
+            spaceBetween: 0
+        },
+
+        300: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+
+    }
+});
+
+
+if (swiperPrev && swiperNext) {
+    swiperPrev.addEventListener('click', () => {
+        mySwiper.slidePrev();
+    })
+    swiperNext.addEventListener('click', () => {
+        mySwiper.slideNext();
+    })
+}
+
+
+// body with burger menu
+
+const btnMenu = document.querySelector('.btn-menu')
+
+if (btnMenu) {
+    btnMenu.addEventListener('click', (e) => {
+        document.body.classList.toggle('body--no-scroll')
+    })
+}
+
+// faq section
+
+const faqArrow = document.querySelectorAll('.faq__question svg')
+const faqAnswer = document.querySelectorAll('.faq__answer')
+
+
+if (faqArrow && faqAnswer) {
+    faqArrow.forEach((arrow, index) => {
+        arrow.addEventListener('click', () => {
+            arrow.classList.toggle('rotate')
+            faqAnswer[index].classList.toggle('faq__answer--visible')
+        })
+    });
+}
